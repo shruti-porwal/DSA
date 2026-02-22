d87fa05e075942b96041ac7a03b82cfa7fb7500b
shruti-porwal <porwalshruti230@gmail.com>
2026-02-22 11:00:13 +0530

    updated

--- LargestElement.java
+++ LargestElement.java
@@ -11,3 +11,16 @@ public class LargestElement {
     }
 }
 // time-complexity - O(n) & space-complexity O(1)
+/*public class LargestElement {
+    public static void main(String[] args) {
+        int[]a= {7,30,63,0,9,-1};
+        int small = 0;
+        for(int i = 0; i<a.length; i++){
+            if(a[i] < small){
+                small = a[i];
+            }
+        }
+        System.out.println(small);
+    }
+    
+}*/
