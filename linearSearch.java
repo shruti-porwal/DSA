8aa5e32655b0b8587cb4fccc2206ec28684d704e
shruti-porwal <porwalshruti230@gmail.com>
2026-03-03 12:22:56 +0530

    initial

--- linearSearch.java
+++ linearSearch.java
@@ -1,12 +1,20 @@
-public class linearSearch {
+public class linearSearch{
     public static void main(String[] args) {
-        int[] arr = {2,6,9,10,46};
+        int[] arr = {2,6,9,10,9,46};
         int target = 9;
+        int index = -1;
         for(int i = 0;i<arr.length;i++){
             if(arr[i] == target){
-                System.out.println(arr[i]);
+                index = i;
+                break;
             }
         }
+            if(index != -1){
+                System.out.println("element is found at" + index);
+            }
+            else{
+                System.out.println("element is not found");
+            }
     }
 }
 // time-complexity - O(n) & space-complexity O(1)
