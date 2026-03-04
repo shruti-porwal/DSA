7fa41e5c58333f71da65b177a76b1a59010d855b
shruti-porwal <porwalshruti230@gmail.com>
2026-03-04 13:25:37 +0530

    inital

---
+++ UnionOFsortedArr.java
@@ -0,0 +1,16 @@
+// by using set approach , which time complexity is 0((n+m)log(n+m)) & space is O(n+m).
+import java.util.*;
+public class UnionOFsortedArr {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {2,4,5,6};
+        Set<Integer> set = new TreeSet<>();
+        for(int num : arr1){
+            set.add(num);
+        }
+        for(int num : arr2){
+            set.add(num);
+        }
+        System.out.println(set);
+    }
+}
