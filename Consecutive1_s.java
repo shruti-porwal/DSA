e12f5d84396a12529bd5f0ea659f3a77bcd21ef4
shruti-porwal <porwalshruti230@gmail.com>
2026-03-09 13:11:56 +0530

    commit

---
+++ Consecutive1_s.java
@@ -0,0 +1,17 @@
+public class Consecutive1_s {
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,1,1};
+        int count = 0;
+        int max_count = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] == 1){
+                count++;
+                max_count = Math.max(max_count,count);
+            }
+            else{
+                count = 0;
+            }
+        }
+        System.out.print(max_count);
+    }
+}
