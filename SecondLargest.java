508db952150f1e0030410a8822e044de3c50dc0c
shruti-porwal <porwalshruti230@gmail.com>
2026-03-12 16:27:33 +0530

    re-structured

--- SecondLargest.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-public class SecondLargest {
-    public static void main(String[] args) {
-        int[] arr = {4,5,6,3,0};
-        Arrays.sort(arr);
-            System.out.println(arr[arr.length-2]);
-        }
-}
-//time-complexity - O(n log n) due to Array.sort() but optimized approach is O(n).
