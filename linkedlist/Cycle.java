2c25d34bbaeb62706c2b1eac88365aaaf7184567
shruti-porwal <porwalshruti230@gmail.com>
2026-05-25 14:01:48 +0530

    added

---
+++ linkedlist/Cycle.java
@@ -0,0 +1,81 @@
+package linkedlist;
+class Node{
+    int data;
+    Node next;
+
+    Node(int val){
+        this.data = val;
+        this.next = null;
+    }
+}
+class List{
+    Node head;
+    Node tail;
+
+    List(){
+        head = tail = null;
+    }
+
+    void push_front(int val){
+        Node newNode = new Node(val);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    void insert(int pos,int val){
+        if(pos<0){
+            System.out.println("LL is empty");
+            return;
+        }
+        if(pos == 0){
+            push_front(val);
+            return;
+        }
+        Node newNode = new Node(val);
+        Node temp = head;
+        for(int i = 0;i<pos-1;i++){
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+    void printLL(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    void Detect_Cycle(){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                System.out.println("Cycle exits");
+            }
+        }
+        System.out.println("no cycle");
+    }
+}
+public class Cycle{
+    public static void main(String[] args) {
+        List l1 = new List();
+        l1.insert(0,3);
+        l1.insert(1, 4);
+        l1.insert(2, 5);
+        l1.insert(3,6);
+        l1.printLL();
+        l1.Detect_Cycle();
+
+    }
+}
+
+
